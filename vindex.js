d9802dd451ae399136028894b3e5f6e1f6fcab82
Thomas Rometsch <thomas.rometsch@gmail.com>
2018-09-04 13:34:34 +0200

    save and load results

--- vindex.js
+++ vindex.js
@@ -26,7 +26,8 @@ function drop_video(e) {
 
     el("mp4").src = window.URL.createObjectURL( files[0] );
 
-	el("log").innerHTML = files[0].name;
+	el("log").innerHTML = "# log for file : " + files[0].name;
+	el("logFileName").value = files[0].name + ".log.txt";
 	
 	el("video").load();
 }
@@ -41,4 +42,52 @@ function registerSleep() {
 
 function registerAwake() {
 	registerState("awake");
+}
+
+// Functions to save textarea from http://cssdeck.com/labs/7bx7mmcm
+
+function saveTextAsFile()
+{
+	var textToWrite = document.getElementById("log").value;
+	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+	var fileNameToSaveAs = document.getElementById("logFileName").value;
+
+	var downloadLink = document.createElement("a");
+	downloadLink.download = fileNameToSaveAs;
+	downloadLink.innerHTML = "Download File";
+	if (window.webkitURL != null)
+	{
+		// Chrome allows the link to be clicked
+		// without actually adding it to the DOM.
+		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+	}
+	else
+	{
+		// Firefox requires the link to be added to the DOM
+		// before it can be clicked.
+		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+		downloadLink.onclick = destroyClickedElement;
+		downloadLink.style.display = "none";
+		document.body.appendChild(downloadLink);
+	}
+
+	downloadLink.click();
+}
+
+function destroyClickedElement(event)
+{
+	document.body.removeChild(event.target);
+}
+
+function loadFileAsText()
+{
+	var fileToLoad = document.getElementById("fileToLoad").files[0];
+
+	var fileReader = new FileReader();
+	fileReader.onload = function(fileLoadedEvent) 
+	{
+		var textFromFileLoaded = fileLoadedEvent.target.result;
+		document.getElementById("log").value = textFromFileLoaded;
+	};
+	fileReader.readAsText(fileToLoad, "UTF-8");
 }
